2ca4ca2f0301d6891dc0204d7f15bf14de93e940
bora park <jemini9595@gmail.com>
2023-07-21 18:28:20 +0900

    Lv.2 호텔예약서비스

---
+++ Lv2/app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,31 @@
+package com.example.reservation_hotel
+
+fun main() {
+    val introduce = "호텔예약 프로그램 입니다"
+    val menu = "[메뉴]"
+    val menuList = "1. 방예약, 2. 예약목록 출력, 3. 예약목록 (정렬), 4. 시스템 종료, 5. 금액 입금,출금 내역 목록 출력, 6. 예약 변경/취소"
+
+    var reservationList = mutableListOf<Reservation>()
+    val reservationManager = ReservationManager(reservationList)
+
+    while (true) {
+        println(introduce)
+        println(menu)
+        println(menuList)
+        println("원하시는 메뉴의 번호를 입력해주세요")
+
+        val selectedNumber = readLine()?.toInt()
+
+        when (selectedNumber) {
+            1 -> reservationManager.reserve()
+            2 -> reservationManager.printReservationList()
+            4 -> {
+                println("시스템을 종료합니다.")
+                return
+            }
+            else -> {
+                println("올바른 메뉴 번호를 선택해주세요.")
+            }
+        }
+    }
+}
