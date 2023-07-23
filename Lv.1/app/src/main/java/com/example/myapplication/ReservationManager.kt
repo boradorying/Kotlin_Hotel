a515c8dacf5ce6447df03ca8f41aa0ceb762f752
bora park <jemini9595@gmail.com>
2023-07-23 18:15:49 +0900

    Lv.1 호텔예약서비스:입력이 완료되면 임의의 금액 호텔예약비 구현완료

--- Lv.1/app/src/main/java/com/example/myapplication/ReservationManager.kt
+++ Lv.1/app/src/main/java/com/example/myapplication/ReservationManager.kt
@@ -1,15 +1,18 @@
 package com.example.myapplication
 
 import java.time.LocalDate
-
+import java.util.Random
 class Reservation(
     val name: String?,
     val phoneSuffix: String,
     val roomNumber: Int,
     val checkIn: Int,
-    val checkout: Int
-
+    val checkout: Int,
+    val initialMoney: Int,
+    val hotelReservationFee: Int
 )
+
+var reservationList = mutableListOf<Reservation>()
 
 class ReservationManager(private val reservationList: MutableList<Reservation>) {
     fun reserve() {
@@ -28,7 +31,8 @@ class ReservationManager(private val reservationList: MutableList<Reservation>) 
                 println("예약하실 방 호실를 입력해주세요 (100 ~ 999)")
                 var myRoomNumber = readLine()!!.toInt()
                 if (myRoomNumber in 100..999) {
-                    val existingReservations = reservationList.filter { it.roomNumber == myRoomNumber }
+                    val existingReservations =
+                        reservationList.filter { it.roomNumber == myRoomNumber }
                     if (existingReservations.isNotEmpty()) {
                         println("해당 호실은 다음 날짜에 예약되어 있습니다:")
                         for (reservation in existingReservations) {
@@ -52,9 +56,22 @@ class ReservationManager(private val reservationList: MutableList<Reservation>) 
                             println("해당 날짜에 이미 방이 예약되어 있습니다. 다시 입력해주세요.")
                         } else {
                             if (myCheckOutDate > myCheckInDate) {
-                                val newReservationList = Reservation(myName, myPhoneSuffix, myRoomNumber, myCheckInDate, myCheckOutDate)
-                                reservationList.add(newReservationList)
+                                val random = Random()
+                                val initialMoney = random.nextInt(900000) + 100000
+                                val hotelReservationFee = 50000 // 호텔 예약비
+                                val newReservation = Reservation(
+                                    myName,
+                                    myPhoneSuffix,
+                                    myRoomNumber,
+                                    myCheckInDate,
+                                    myCheckOutDate,
+                                    initialMoney,
+                                    hotelReservationFee
+                                )
+                                reservationList.add(newReservation)
                                 println("예약이 완료되었습니다.")
+                                println("나의 초기머니: ${initialMoney}원")
+                                println("호텔 예약비로 인해 빠져나간 금액: ${hotelReservationFee}원")
                             } else {
                                 println("올바른 체크아웃 날짜를 입력해주세요.")
                             }
